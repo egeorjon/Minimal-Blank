9f5a08f1c0d359068bfc726d08b3cf570bae0b34
egeorjon <egeorjon@gmail.com>
2021-03-21 21:23:34 +0100

    Initial commit

---
+++ static/assets/js/search.js
@@ -0,0 +1,131 @@
+
+  var fuseOptions = {
+    shouldSort        : true,
+    includeScore      : false,
+    includeMatches    : true,
+    isCaseSensitive   : false,
+    threshold         : 0.95,
+    tokenize          : true,
+    location          : 0,
+    distance          : 100,
+    minMatchCharLength: 3,
+    maxPatternLength  : 32,
+    minMatchCharLength: 3,
+    keys: [
+      { name:"title",    weight:0.8 },
+      { name:"contents", weight:0.5 },
+      { name:"tags",     weight:0.3 }
+    ]
+  }
+
+  var searchQuery = param("s");
+  if (searchQuery) {
+   
+    document.querySelector("#search-query").value = searchQuery;
+
+    var fuse = new Fuse(searchIndex, fuseOptions);
+    var results = fuse.search(searchQuery);
+    document.querySelector("#search-results").classList.remove("d-none");
+    if (results.length == 0) {
+      insertMsg(document.querySelector("#search-results-title"), "Aucun article ne correspond aux critères - No post found, with this criteria" );
+    }
+    else {
+      /* console.log({"matches":results}); */
+      populateResults(results);
+    }
+  }
+  else {
+    insertMsg(document.querySelector("#search-query"), "Veuillez saisir vos critères de recherches - Please enter what you are looking for." );
+  }
+
+  /* ------------------------------------------------------
+     --- Display the results
+     ------------------------------------------------------ */
+  function populateResults(results) {
+      
+    var templateDefinition = document.querySelector("#search-result-template").textContent;
+    var items = document.querySelector("#search-results-items");
+
+    results.forEach( (value, key) => {
+      /*  console.log({"Key":key});
+        console.log({"Value": value}); */
+
+        if ( value.item.title != "Recherche" && value.item.title != "Search" && value.item.title != "Catalog" ) {
+          var output = render(
+            templateDefinition, {
+              key       : key,
+              title     : value.item.title,
+              link      : value.item.permalink,
+              tags      : value.item.tags,
+              categories: value.item.categories,
+              publish   : value.item.publish,
+              section   : value.item.section,
+              reading   : value.item.reading,
+              summary   : truncate(value.item.contents,50) + "..."
+              }
+            );
+          items.innerHTML = items.innerHTML + output;
+        }
+      }
+    );
+  }
+
+  /* ------------------------------------------------------
+     --- 
+     ------------------------------------------------------ */
+  function render(templateString, data) {
+    var conditionalMatches,conditionalPattern,copy;
+
+    conditionalPattern = /\$\{\s*isset ([a-zA-Z]*) \s*\}(.*)\$\{\s*end\s*}/g;
+    //since loop below depends on re.lastInxdex, we use a copy to capture any manipulations whilst inside the loop
+    copy = templateString;
+    while ((conditionalMatches = conditionalPattern.exec(templateString)) !== null) {
+      if( data[conditionalMatches[1]] ){
+        //valid key, remove conditionals, leave contents.
+        copy = copy.replace(conditionalMatches[0],conditionalMatches[2]);
+      }else{
+        //not valid, remove entire section
+        copy = copy.replace(conditionalMatches[0],'');
+      }
+    }
+    templateString = copy;
+    //now any conditionals removed we can do simple substitution
+    var key, find, re;
+    for (key in data) {
+        find = '\\$\\{\\s*' + key + '\\s*\\}';
+        re = new RegExp(find, 'g');
+        templateString = templateString.replace(re, data[key]);
+    }
+    return templateString;
+  }
+
+  /* ------------------------------------------------------
+     --- 
+     ------------------------------------------------------ */
+  function param(name) {
+      return decodeURIComponent((location.search.split(name + '=')[1] || '').split('&')[0]).replace(/\+/g, ' ');
+  }
+
+  /* ------------------------------------------------------
+     --- 
+     ------------------------------------------------------ */
+  function insertMsg(referenceNode, msg) {
+    var newNode = document.createElement("p");
+    newNode.textContent = msg;
+    newNode.classList.add("search-msg");
+    insertAfter(referenceNode, newNode);
+  }
+
+  /* ------------------------------------------------------
+     --- 
+     ------------------------------------------------------ */
+  function insertAfter(referenceNode, newNode) {
+    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+  }
+
+  /* ------------------------------------------------------
+     --- 
+     ------------------------------------------------------ */
+  function truncate(str, no_words) {
+    return str.split(" ").splice(0,no_words).join(" ");
+  }
